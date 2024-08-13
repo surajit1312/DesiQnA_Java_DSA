427550cb699a7b852db009e9d626581892d110e9
surajitrana <surajit1602@gmail.com>
2024-08-14 02:02:24 +0530

    fix[folder-modified]: restructure

--- Hashing/Starter/Hashing_01.java
+++ Hashing/Starter/Hashing_01.java
@@ -1,33 +1,33 @@
-package Hashing;
+package Hashing.Starter;
 
 import java.util.Scanner;
 
-/**
- * Better approach
- * 
- * TC: O(N + Q)
- * SC: O(max) where max = maximum value in nums array
- * 
- * @param args
- */
-public class Hashing_02 {
+public class Hashing_01 {
+    /**
+     * Brute-Force approach
+     * 
+     * TC: O(Q x N)
+     * SC: O(1)
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int[] nums = new int[n];
-        int max = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) { // TC: O(N) - taking input TC is not considered
             nums[i] = scn.nextInt();
-            max = Math.max(max, nums[i]);
-        }
-        int[] freq = new int[max + 1]; // SC: O(max + 1)
-        for (int i = 0; i < n; i++) { // TC: O(N)
-            freq[nums[i]]++;
         }
         int q = scn.nextInt();
         for (int i = 0; i < q; i++) { // TC: O(Q)
             int query = scn.nextInt();
-            System.out.print(freq[query] + " ");
+            int count = 0;
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                if (nums[j] == query) {
+                    count++;
+                }
+            }
+            System.out.print(count + " ");
         }
         scn.close();
     }
--- Hashing/Starter/Hashing_03.java
+++ Hashing/Starter/Hashing_03.java
@@ -1,4 +1,4 @@
-package Hashing;
+package Hashing.Starter;
 
 import java.util.HashMap;
 import java.util.Map;
